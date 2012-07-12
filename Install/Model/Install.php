2171b27ad813177a8c19aaa78bbb8359b38e25ef
Attila Fulop <fulopattila122@gmail.com>
2012-07-12 21:25:39 +0300

    Changed the installer to be able to selectively generate models and/or database only

--- Install/Model/Install.php
+++ Install/Model/Install.php
@@ -9,7 +9,7 @@
  * @copyright   Copyright (c) 2011 - 2012 Attila Fülöp
  * @author      Attila Fülöp
  * @license     GNU LGPL v3 http://www.opensource.org/licenses/lgpl-3.0.html
- * @version     $Revision-Id$ $Date$
+ * @version     5 2012-07-12
  * @since       2011-12-12
  *
  */
@@ -26,7 +26,16 @@ class Konekt_Framework_Install_Model_Install
    const SCHEMA_FILE    = 'schema.yml';
    const FIXTURE_FILE   = 'data.yml';
 
-   public function installModule($moduleName, $skipDbOperations = false)
+   /**
+    * Installs a single Module
+    *
+    * @param   string   $moduleName       The name of the module to install
+    * @param   bool     $doDbOperations   If true database will be created (from models) and fixtures will be loaded (from data.yml)
+    * @param   bool     $generateModels   If true the Model classes will be (re)generated from schema.yml
+    *
+    * @return  bool     Returns true if operation was successful, false on failure.
+    */
+   public function installModule($moduleName, $doDbOperations = true, $generateModels = true)
    {
       try
       {
@@ -37,14 +46,15 @@ class Konekt_Framework_Install_Model_Install
          /* Install Schema */
          if (file_exists($confDir . DS . self::SCHEMA_FILE))
          {
-            Doctrine_Core::generateModelsFromYaml($confDir . DS . self::SCHEMA_FILE,
-               $modelsDir);
-            if (!$skipDbOperations)
+            if ($generateModels)
+               Doctrine_Core::generateModelsFromYaml($confDir . DS . self::SCHEMA_FILE, $modelsDir);
+               
+            if ($doDbOperations)
                Doctrine_Core::createTablesFromModels($modelsDir);
          }
          
          /* Install Initial Data fixtures */
-         if (!$skipDbOperations && file_exists($confDir . DS . self::FIXTURE_FILE))
+         if ($doDbOperations && file_exists($confDir . DS . self::FIXTURE_FILE))
          {
             Konekt::app()->connection->exec('set names utf8');
             Doctrine_Core::loadData($confDir . DS . self::FIXTURE_FILE);
--- Install/controllers/install.php
+++ Install/controllers/install.php
@@ -16,33 +16,46 @@ require_once("../../Konekt.php");
    /**
     * Installs a single Application Module
     *
-    * @param string $module The name of the module
-    * @param Konekt_Install_Model_Install $installer The Installer Model instance
-    * @param bool $omitSql Whether or not to skip Database operations
+    * @param string                       $module              The name of the module
+    * @param Konekt_Install_Model_Install $installer           The Installer Model instance
+    * @param bool                         $installDatabase     Whether or not to do Database operations
+    * @param bool                         $generateModels      Whether or not to generate Model classes
     *
-    * @return bool Returns the value that was returned by the Installer Model instance
+    * @return bool                        Returns the value that was returned by the Installer Model instance
     */
    
-   function _installModule($module, $installer, $omitSql)
+   function _installModule($module, $installer, $installDatabase, $generateModels)
    {
-      echo "Installing module $module...";
-      $result = $installer->installModule($module, $omitSql);
+      if ($installDatabase) {
+         $opText = 'Installing';
+         $opText .= $generateModels ? ' and generating models for' : ' database for';
+      } else {
+         if ($generateModels) {
+            $opText = 'Generating models for';  
+         } else {
+            echo "No operation specified for $module, skipping.\n";
+            return false;
+         }
+      }
+      
+      echo "$opText module $module...";
+      $result = $installer->installModule($module, $installDatabase, $generateModels);
       echo $result ? "OK.\n" : "FAILED.\n";      
       return $result;
    }
 
-   function install($omitSql = false, $singleModule = null)
+   function install($doSql, $doModels, $singleModule = null)
    {
       $installer = new Konekt_Framework_Install_Model_Install();
       
       if ($singleModule)
       {
-         _installModule($singleModule, $installer, $omitSql);
+         _installModule($singleModule, $installer, $doSql, $doModels);
       }
       else
       {
          foreach (Konekt::app()->getModules() as $module => $params)
-            _installModule($module, $installer, $omitSql);
+            _installModule($module, $installer, $doSql, $doModels);
       }
    }
    
@@ -54,50 +67,40 @@ require_once("../../Konekt.php");
    
    function displayHelp()
    {
-      echo "Konekt Framework Command Line Installer.\nUsage:
-         \t-r:\tReinstall (drops database and installs)
-         \t-i:\tInstall: Generates Models & DB Tables from Schema and loads fixtures
-         \t-l:\tList Modules: Display the list of the Module Names
-         \t-h:\tDisplay this help text
-         \t--nosql:\tOnly generate models, do not touch the database
-         \t--module <modulename>\tOnly install a single module
-      ";   
+      echo "Konekt Framework Command Line Installer.\nUsage:\n";
+      echo "   -i:\tInstall: Generates DB Tables from Schema and loads fixtures\n";
+      echo "   -d:\tDrop database: Drop database (before install)\n";
+      echo "   -g:\tGenerate Model Classes from yaml\n";
+      echo "   -l:\tList Modules: Display the list of the Module Names\n";
+      echo "   -h:\tDisplay this help text\n";
+      echo "   --module <modulename>\tOnly install a single module\n";        
    }
 
    if (Konekt::app()->runningFromCli())
    {
-      $opt = getopt("rhil", array("module:", "nosql"));
+      $opt = getopt("idglh", array("module:"));
       
-      if (empty($opt) || isset($opt['h']))
-      {
+      if (empty($opt) || isset($opt['h'])) {
          displayHelp();
          exit(0);
       }
       
-      if (isset($opt['l']))
-      {
+      
+      if (isset($opt['l'])) {
          displayModuleList();
          exit(0);
       }
       
-      /* FULL REINSTALL */
-      if (isset($opt['r']))
-      {
-         if (isset($opt['nosql']))
-            die("--nosql and -r are incompatible options\n");
-            
-         recreateDatabase();
-         install();
-         exit(0);
-      }
-      /* INSTALL ONLY */
-      elseif (isset($opt['i']))
-      {
-         $module = isset($opt['module']) ? $opt['module'] : null;
-         install(isset($opt['nosql']), $module);
-         exit(0);
-      }
+      $module = isset($opt['module']) ? $opt['module'] : null;
       
-      exit("You did not specify any command\n");
-
+      if (isset($opt['d'])) recreateDatabase();
+      
+      $doSql    = isset($opt['i']);
+      $doModels = isset($opt['g']);
+      if ($doModels || $doSql) {
+         install($doSql, $doModels, $module);
+         exit(0);
+      } elseif (!isset($opt['d'])) {
+         exit("You did not specify any command\n");
+      }
    }
