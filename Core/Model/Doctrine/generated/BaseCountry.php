3eeb34653f9c33bd54e941b04fdf77d905915c86
Attila Fulop <fulopattila122@gmail.com>
2012-07-28 11:08:20 +0300

    Fix bug #969144 changed collation from utf8_hungarian_ci to utf8_unicode_ci

--- Core/Model/Doctrine/generated/BaseCountry.php
+++ Core/Model/Doctrine/generated/BaseCountry.php
@@ -38,7 +38,7 @@ abstract class BaseCountry extends Doctrine_Record
              ));
 
         $this->option('type', 'INNODB');
-        $this->option('collate', 'utf8_hungarian_ci');
+        $this->option('collate', 'utf8_unicode_ci');
         $this->option('charset', 'utf8');
     }
 
