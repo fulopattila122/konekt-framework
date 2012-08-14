7cf772da06de4c0078ae59b38373a8128aee703b
Attila Fulop <fulopattila122@gmail.com>
2012-08-14 15:47:41 +0300

    Added the Abstract Entity class

---
+++ Core/Model/Entity/Abstract.php
@@ -0,0 +1,111 @@
+<?php
+/**
+ * Abstract.php contains the Abstract Entity Model Class
+ *
+ * @category    Konekt
+ * @package     Framework
+ * @subpackage  Core
+ * @copyright   Copyright (c) 2012 Attila Fulop
+ * @author      Attila Fulop
+ * @license     GNU LGPL v3 http://www.opensource.org/licenses/lgpl-3.0.html
+ * @version     1 2012-07-29
+ * @since       2012-07-29
+ *
+ */
+
+
+/**
+ * The Common Entity Abstract model class for wrapping Doctrine Entities
+ *
+ * @caegory    Konekt
+ * @package    Framework
+ * @subpackage Core
+ */
+abstract class Konekt_Framework_Core_Model_Entity_Abstract {
+   
+   /** This const must be overwritten by concrete classes */
+   const ENTITY_NAME = '';
+   
+   /** @var Doctrine_Record The internal underlying Doctrine_Record derived class instance */
+   protected $_entity;
+   
+   /**
+    * Checks whether the internal Doctrine Entity is initialized and creates a new Instance if necessary
+    *
+    * @return bool True if record existed prior to the function call, false if it was initialized during the call
+    */
+   private function _checkRecordInstance()
+   {
+      if (!$this->_entity)
+      {
+         $this->_entity = new Issue();
+         return false;
+      }
+      else
+         return true;      
+   }
+   
+   
+   public function getId()
+   {
+      return $this->_entity ? $this->_entity->id : NULL;
+   }
+   
+      
+   /**
+    * Loads an Entity by its id
+    *
+    * @return Konekt_Framework_Core_Model_Entity_Abstract|false  Returns the Instance or false if failed to load
+    */
+   public function load($id)
+   {
+      $this->_checkRecordInstance();
+      if ($this->_entity->load($id)) {
+         return $this;
+      } else {
+         return false;
+      }
+   }
+   
+   
+   /**
+    * Saves the changes to the storage database
+    *
+    * @return Konekt_Framework_Core_Model_Entity_Abstract  Returns the self reference
+    */
+   public function save()
+   {
+      if ($this->_checkRecordInstance())
+      {
+         $this->_entity->save();
+      }
+      return $this;
+   }
+   
+   /**
+    * Deletes the Entity
+    *
+    * @return bool   Returns true if deleted successfully
+    */
+   public function delete()
+   {
+      if ($this->_checkRecordInstance()) {
+         return $this->_entity->delete();
+      } else {
+         return false;
+      }
+   }
+   
+   
+   /**
+    * Checks whether an entity with the given id exists
+    *
+    * @return bool
+    */
+   public static function exists($id)
+   {
+      $entity = Doctrine_Core::getTable(self::ENTITY_NAME)->find($id);
+      return $entity ? true : false;
+   }
+   
+}
